3c290c17914e82006d581b20331ecca1a65229f1
fallenatlas <tiago.a.p.antunes@gmail.com>
2025-04-05 10:11:21 +0100

    bit better character movement and change base project settings

--- Scripts/nightmare_character.gd
+++ Scripts/nightmare_character.gd
@@ -1,19 +1,17 @@
 extends CharacterBody2D
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("sleep_toggle"):
 		self.queue_free()
 	
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction : Vector2 = Vector2(Input.get_axis("sleep_left", "sleep_right"), Input.get_axis("sleep_up", "sleep_down"))
-	if direction:
-		velocity = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.y = move_toward(velocity.y, 0, SPEED)
+	var direction : Vector2
+	var input = Input.get_vector("sleep_left", "sleep_right", "sleep_up", "sleep_down")
+	if input.length() != 0:
+		direction = input
+		
+	velocity = velocity.lerp(input * SPEED, delta*3)
 
 	move_and_slide()
