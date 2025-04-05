80412e766778fa41e98f090d1ad53fd97812e395
fallenatlas <tiago.a.p.antunes@gmail.com>
2025-04-06 00:50:02 +0100

    add fork

--- Scripts/damaging.gd
+++ Scripts/damaging.gd
@@ -1,13 +1,14 @@
 class_name Damaging
 extends Area2D
 
-@onready var anim : AnimationPlayer = $"../AnimationPlayer"
+@export_file("*.tscn") var level_to_reload
+
+@onready var anim : AnimationPlayer = $AnimationPlayer
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Players"):
 		#TODO Take damage
-		print("Die")
-		#body.Die()
+		reload_level()
 
 func activate() -> void:
 	#In traps it is the contrary (activate is to make them disappear)
@@ -16,3 +17,7 @@ func activate() -> void:
 func deactivate() -> void:
 	#In traps it is the contrary (deactivate is to make them appear)
 	anim.play("disappear")
+
+func reload_level():
+	print("loading level : " + level_to_reload)
+	get_tree().change_scene_to_file(level_to_reload)
