aa2ca1643e6e0c1958c1a524eb9dd68d58c80b0f
JoaoAlmeida20 <redstone113@gmail.com>
2025-04-05 21:42:18 +0200

    animations for both chars and misc polish

--- Scripts/eat.gd
+++ Scripts/eat.gd
@@ -1,10 +1,18 @@
 extends CharacterBody2D
 
-const SPEED = 130.0
-const JUMP_VELOCITY = -300.0
+const SPEED = 280.0
+const ACCEL = 2500.0
+const JUMP_VELOCITY = -400.0
+const FRICTION = 3000.0
 
-const SPIT_SPEED_X = 600.0
+const EAT_STARTUP = 0.5
+
+const SPIT_SPEED_X = 1000.0
 const SPIT_SPEED_Y = -350.0
+const SPIT_OFFSET_X = 50.0
+const SPIT_OFFSET_Y = -80.0
+
+const PRIORITY_ANIMS = ["land", "eat", "spit"]
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -15,6 +23,10 @@ var eat_area_shape: CollisionShape2D
 var sleepy_available = false
 var sleepy_ref: Sleepy = null
 var sleepy_eaten = false
+var eat_time = 0.0
+var is_eating = false
+
+var was_grounded = true
 
 var facing = 1
 
@@ -24,47 +36,72 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	# Gravity
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Jump
-	if Input.is_action_just_pressed("eat_up") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+func _physics_process(delta: float) -> void:
+	if is_on_floor() and not was_grounded:
+		animated_sprite.play("land")
 		
 	# Eat
-	if Input.is_action_just_pressed("eat_action"):
+	if Input.is_action_just_pressed("eat_action") and not is_eating:
 		# Swallow sleepy
 		if not sleepy_eaten:
-			if sleepy_available and sleepy_ref and sleepy_ref.is_sleeping:
-				sleepy_ref.process_mode = Node.PROCESS_MODE_DISABLED
-				sleepy_eaten = true
-				sleepy_ref.visible = false
+			animated_sprite.play("eat")
+			eat_time = 0.0
+			is_eating = true
 		# Spit sleepy
 		elif sleepy_eaten:
+			animated_sprite.play("spit")
 			sleepy_ref.process_mode = Node.PROCESS_MODE_INHERIT
 			sleepy_eaten = false
 			sleepy_ref.position = self.position
-			sleepy_ref.position.y -= 1
+			sleepy_ref.position.x += SPIT_OFFSET_X * facing
+			sleepy_ref.position.y += SPIT_OFFSET_Y
 			sleepy_ref.velocity = Vector2(SPIT_SPEED_X * facing, SPIT_SPEED_Y)
 			sleepy_ref.got_spit = true
 			sleepy_ref.visible = true
+	eat_time += delta
+	if eat_time > EAT_STARTUP and is_eating:
+		is_eating = false
+		if sleepy_available and sleepy_ref and sleepy_ref.is_sleeping:
+			sleepy_ref.process_mode = Node.PROCESS_MODE_DISABLED
+			sleepy_eaten = true
+			sleepy_ref.visible = false
 		
 	# Movement
 	var direction = Input.get_axis("eat_left", "eat_right")
 	var previous_facing = facing
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = move_toward(velocity.x, direction * SPEED, ACCEL * delta)
 		facing = 1 if direction > 0 else -1
-		animated_sprite.play("walk")
 		animated_sprite.flip_h = false if direction > 0 else 1
+		if (is_on_floor() and 
+		(animated_sprite.animation not in PRIORITY_ANIMS or not animated_sprite.is_playing())):
+			animated_sprite.play("walk")
 	else:
-		animated_sprite.stop()
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if not is_on_floor():
+			velocity.x = move_toward(velocity.x, direction, FRICTION * delta)
+		else:		
+			velocity.x = move_toward(velocity.x, direction, FRICTION * delta)
+			if animated_sprite.animation not in PRIORITY_ANIMS or not animated_sprite.is_playing():
+				animated_sprite.play("idle")
 	
 	if facing != previous_facing:
 		eat_area_shape.position.x *= -1
+		
+	# Handle jump.
+	if Input.is_action_just_pressed("eat_up") and is_on_floor():
+		animated_sprite.play("jump")
+		velocity.y = JUMP_VELOCITY
+		
+	# Gravity
+	if not is_on_floor():
+		if velocity.y > 0 and animated_sprite.animation != "fall":
+			animated_sprite.play("fall")
+		velocity.y += gravity * delta
+	
+	if is_on_floor():
+		was_grounded = true
+	else:
+		was_grounded = false
 	
 	move_and_slide()
 
--- Scripts/sleep_character.gd
+++ Scripts/sleep_character.gd
@@ -1,13 +1,19 @@
 class_name Sleepy extends CharacterBody2D
 
-const SPEED = 500.0
-const JUMP_VELOCITY = -400.0
-const FRICTION = 10.0
+const SPEED = 220.0
+const ACCEL = 2000.0
+const JUMP_VELOCITY = -300.0
+const SPIT_FRICTION = 10.0
+const GROUND_FRICTION = 12000.0
+const AIR_FRICTION = 4000.0
+
+const SLEEP_OFFSET_Y = 42.0
 
 var nightmare_character_scene = preload("res://Scenes/nightmare_character.tscn")
 
 var is_sleeping = false
 var got_spit = false
+var was_grounded = true
 
 @onready var animated_sprite = $AnimatedSprite2D
 @onready var collision_shape = $CollisionShape2D
@@ -25,13 +31,17 @@ func _physics_process(delta: float) -> void:
 
 func process_sleeping(delta : float) -> void:
 	if Input.is_action_just_pressed("sleep_toggle"):
+		animated_sprite.position.y -= SLEEP_OFFSET_Y
 		is_sleeping = false
+		
+	if not animated_sprite.is_playing():
+		animated_sprite.play("sleeping")
 
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	if got_spit:
-		velocity.x = move_toward(velocity.x, 0, FRICTION)
+		velocity.x = move_toward(velocity.x, 0, SPIT_FRICTION)
 		# End spit physics once you stop
 		if velocity.length_squared() < 0.01:
 			got_spit = false
@@ -40,37 +50,48 @@ func process_sleeping(delta : float) -> void:
 	
 	
 func process_awake(delta : float) -> void:
-	if Input.is_action_just_pressed("sleep_toggle"):
-		is_sleeping = true
-		velocity = Vector2(0, 0)
-		spawn_nightmare()
+	if is_on_floor() and not was_grounded:
+		animated_sprite.play("land")
 
-	var velocity_y = velocity.y
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("sleep_left", "sleep_right")
 	var temp_v = Vector2(direction, 0)
 	if direction:
-		velocity = velocity.lerp(temp_v * SPEED, delta*2)
-		#velocity.x = direction * SPEED
-		animated_sprite.play("default")
+		velocity.x = move_toward(velocity.x, temp_v.x * SPEED, ACCEL * delta)
 		animated_sprite.flip_h = true if direction < 0 else false
+		if is_on_floor() and (animated_sprite.animation != "land" or not animated_sprite.is_playing()):
+			animated_sprite.play("walk")
 	else:
-		animated_sprite.stop()
 		if not is_on_floor():
-			velocity = velocity.lerp(temp_v * SPEED, delta*2)
-		else:
-			velocity = velocity.lerp(temp_v * SPEED, delta*6)
-			#velocity.x = move_toward(velocity.x, 0, SPEED)
-		
-	velocity.y = velocity_y
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
+			velocity.x = move_toward(velocity.x, temp_v.x, AIR_FRICTION * delta)
+		else:		
+			velocity.x = move_toward(velocity.x, temp_v.x, GROUND_FRICTION * delta)
+			if not animated_sprite.animation == "land" or not animated_sprite.is_playing():
+				animated_sprite.play("idle")
+	
 	# Handle jump.
 	if Input.is_action_just_pressed("sleep_up") and is_on_floor():
+		animated_sprite.play("jump")
 		velocity.y = JUMP_VELOCITY
+		
+	# Add the gravity.
+	if not is_on_floor():
+		if velocity.y > 0:
+			animated_sprite.play("fall")
+		velocity += get_gravity() * delta
+		
+	if is_on_floor():
+		was_grounded = true
+	else:
+		was_grounded = false
+	
+	if Input.is_action_just_pressed("sleep_toggle"):
+		is_sleeping = true
+		animated_sprite.play("falling_asleep")
+		animated_sprite.position.y += SLEEP_OFFSET_Y
+		velocity = Vector2(0, 0)
+		spawn_nightmare()
 
 func spawn_nightmare() -> void:
 	var nightmare_character = nightmare_character_scene.instantiate()
