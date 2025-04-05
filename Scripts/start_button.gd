c2ec94b0764593e4dee8e684731eb366d3d399b7
fallenatlas <tiago.a.p.antunes@gmail.com>
2025-04-05 12:24:10 +0100

    add main menu

---
+++ Scripts/start_button.gd
@@ -0,0 +1,18 @@
+extends Button
+
+@export_file("*.tscn") var level_to_load
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_pressed() -> void:
+	change_level()
+
+func change_level():
+	print("loading level : " + level_to_load)
+	get_tree().change_scene_to_file(level_to_load)
